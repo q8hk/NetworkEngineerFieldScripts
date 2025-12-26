e4804d3f79fbaf8f9d6a5114d0577b293ba6ffa9
Q8HK <delpieroq7@gmail.com>
2025-12-26 15:37:16 +0300

    Add delay between iterations in IP conflict detection loop

--- ip_conflict_detect.cmd
+++ ip_conflict_detect.cmd
@@ -41,6 +41,7 @@ set "PING_FAILURES=0"
 for /L %%S in (1,1,%MAX_SECONDS%) do (
     call :tick %%S
     if "!RESULT!"=="CONFLICT" goto :finish_loop
+    timeout /t 1 /nobreak >nul
 )
 
 :finish_loop
